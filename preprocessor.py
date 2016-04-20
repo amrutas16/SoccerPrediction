2a67d76eb554ca7c99993d89061c8def2d0f8154
Amruta Shintre <amruta.s16@gmail.com>
2016-04-19 21:35:32 -0400

    Modifications

--- preprocessor.py
+++ preprocessor.py
@@ -2,13 +2,14 @@ import pandas as pd
 import os
 import csv
 from collections import defaultdict
-k = 4
+k = 7
+test_classes = []
 
 
 def get_features(d):
     dir = os.getcwd() + '/data/' + d
     filenames = os.listdir(dir)
-   
+
     data = []
     features = []
     class_labels = []
@@ -27,15 +28,18 @@ def get_features(d):
                     ,at_match_count[away_team], row[6]]
                 data.append(l)
 
-        data = pd.DataFrame(data, columns=('HomeTeam', 'AwayTeam', 'FTHG', 'FTAG', 'HST', 'AST', 'HC', 'AC', 'HTMN', 'ATMN', 'FTR'))
+        data = pd.DataFrame(data, columns=('HomeTeam', 'AwayTeam', 'FTHG', 'FTAG', 'HST', 'AST', 'HC', 'AC', 'HTMN',
+                                           'ATMN', 'FTR'))
 
         home_goals, home_corners, home_st, away_goals, away_corners, away_st = create_dictionaries(data)
 
         if d == 'train':
-            features, class_labels = create_train_features(home_goals, home_corners, home_st, away_goals, away_corners, away_st, data, features, class_labels)
+            features, class_labels = create_train_features(home_goals, home_corners, home_st, away_goals, away_corners,
+                                                           away_st, data, features, class_labels)
 
         if d == 'test':
-            features, class_labels = create_test_features(home_goals, home_corners, home_st, away_goals, away_corners, away_st, data, features, class_labels)
+            features, class_labels = create_test_features(home_goals, home_corners, home_st, away_goals, away_corners,
+                                                          away_st, data, features, class_labels)
 
     return features, class_labels
 
@@ -65,11 +69,13 @@ def create_dictionaries(data):
     return home_goals, home_corners, home_st, away_goals, away_corners, away_st
 
 
-def create_test_features(home_goals, home_corners, home_st, away_goals, away_corners, away_st, data, features, class_labels):
+def create_test_features(home_goals, home_corners, home_st, away_goals, away_corners, away_st, data, features,
+                         class_labels):
     for index, row in data.iterrows():
         home_match_no = row['HTMN']
         away_match_no = row['ATMN']
         result = row['FTR']
+
         if home_match_no <= k or away_match_no <= k:
             continue
 
@@ -83,14 +89,21 @@ def create_test_features(home_goals, home_corners, home_st, away_goals, away_cor
         away_st_avg = 0
 
         for i in range(home_match_no - k - 1, home_match_no - 1):
-            home_g_avg += home_goals[home_team][i] / k
-            home_c_avg += home_corners[home_team][i] / k
-            home_st_avg += (home_st[home_team][i]) / k
+            home_g_avg += home_goals[home_team][i]
+            home_c_avg += home_corners[home_team][i]
+            home_st_avg += (home_st[home_team][i])
 
         for i in range(away_match_no - k - 1, away_match_no - 1):
-            away_g_avg += away_goals[away_team][i] / k
-            away_c_avg += away_corners[away_team][i] / k
-            away_st_avg += away_st[away_team][i] / k
+            away_g_avg += away_goals[away_team][i]
+            away_c_avg += away_corners[away_team][i]
+            away_st_avg += away_st[away_team][i]
+
+        home_g_avg = float(home_g_avg) / float(k)
+        away_g_avg = float(home_g_avg) / float(k)
+        home_c_avg = float(home_g_avg) / float(k)
+        away_c_avg = float(home_g_avg) / float(k)
+        home_st_avg = float(home_g_avg) / float(k)
+        away_st_avg = float(home_g_avg) / float(k)
 
         if home_st_avg == 0:
             h = 0.0
@@ -105,43 +118,51 @@ def create_test_features(home_goals, home_corners, home_st, away_goals, away_cor
         l = [home_g_avg - away_g_avg, home_c_avg - away_c_avg, h - a]
         features.append(l)
         class_labels.append(result)
-
     return features, class_labels
 
 
-def create_train_features(home_goals, home_corners, home_st, away_goals, away_corners, away_st, data, features, class_labels):
+def create_train_features(home_goals, home_corners, home_st, away_goals, away_corners, away_st, data, features,
+                          class_labels):
     for index, row in data.iterrows():
-        # home_match_no = row['HTMN']
-        # away_match_no = row['ATMN']
         result = row['FTR']
         # if home_match_no <= k or away_match_no <= k:
         #     continue
 
+        home_match_no = row['HTMN']
+        away_match_no = row['ATMN']
+
         home_team = row['HomeTeam']
         away_team = row['AwayTeam']
-        hg = int(row['FTHG'])
-        ag = int(row['FTAG'])
-        hc = int(row['HC'])
-        ac = int(row['AC'])
-        hst = int(row['HST'])
-        ast = int(row['AST'])
+        # hg = int(row['FTHG'])
+        # ag = int(row['FTAG'])
+        # hc = int(row['HC'])
+        # ac = int(row['AC'])
+        # hst = int(row['HST'])
+        # ast = int(row['AST'])
 
-        # home_g_avg = 0
-        # away_g_avg = 0
-        # home_c_avg = 0
-        # away_c_avg = 0
-        # home_st_avg = 0
-        # away_st_avg = 0
+        home_g_avg = 0
+        away_g_avg = 0
+        home_c_avg = 0
+        away_c_avg = 0
+        home_st_avg = 0
+        away_st_avg = 0
 
-        # for i in range(home_match_no - k - 1, home_match_no - 1):
-        #     home_g_avg += home_goals[home_team][i] / k
-        #     home_c_avg += home_corners[home_team][i] / k
-        #     home_st_avg += home_st[home_team][i] / k
-        #
-        # for i in range(away_match_no - k - 1, away_match_no - 1):
-        #     away_g_avg += away_goals[away_team][i] / k
-        #     away_c_avg += away_corners[away_team][i] / k
-        #     away_st_avg += away_st[away_team][i] / k
+        for i in range(home_match_no - k - 1, home_match_no - 1):
+            home_g_avg += home_goals[home_team][i] / k
+            home_c_avg += home_corners[home_team][i] / k
+            home_st_avg += home_st[home_team][i] / k
+
+        for i in range(away_match_no - k, away_match_no):
+            away_g_avg += away_goals[away_team][i] / k
+            away_c_avg += away_corners[away_team][i] / k
+            away_st_avg += away_st[away_team][i] / k
+
+        hg = home_g_avg
+        ag = away_g_avg
+        hc = home_c_avg
+        ac = away_c_avg
+        hst = home_st_avg
+        ast = away_st_avg
 
         if hst == 0:
             h = 0.0
@@ -158,7 +179,4 @@ def create_train_features(home_goals, home_corners, home_st, away_goals, away_co
         class_labels.append(result)
 
     return features, class_labels
-
-
-
 
--- soccerPrediction.py
+++ soccerPrediction.py
@@ -1,5 +1,6 @@
 import preprocessor
 import logisticRegression
+import randomForest
 
 
 def main():
@@ -8,21 +9,42 @@ def main():
 
     X_test, y_test = preprocessor.get_features('test')
     pred_probabilities = mlr_model.predict_proba(X_test)
-    classes = mlr_model.classes_
-    pred_y_test = []
-
-    for p in pred_probabilities:
-        class_index = get_class_index(p)
-        pred_y_test.append(classes[class_index])
+    pred_classes = mlr_model.predict(X_test)
 
     t = 0
-    for i in range(len(y_test)):
-        if y_test[i] == pred_y_test[i]:
+    for i in range(len(pred_classes)):
+        if pred_classes[i] == y_test[i]:
             t += 1
+
+    classes = mlr_model.classes_
+
+    pred_y_test = []
+
+    # for p in pred_probabilities:
+    #     class_index = get_class_index(p)
+    #     pred_y_test.append(classes[class_index])
+
+    # t = 0
+    # for i in range(len(y_test)):
+    #     if y_test[i] == pred_y_test[i]:
+    #         t += 1
 
     print t
     print len(y_test)
     print float(t)/float(len(y_test))
+
+    #
+    # ##### Random forest
+    # rf_model = randomForest.random_forest_classifier(X_train, y_train)
+    # pred_y_test = rf_model.predict(X_test)
+    #
+    # t = 0
+    # for i in range(len(y_test)):
+    #     if pred_y_test[i] == y_test[i]:
+    #         t += 1
+    #
+    # print t
+    # print float(t)/float(len(y_test))
 
 
 def get_class_index(p):
@@ -32,7 +54,7 @@ def get_class_index(p):
     for i in range(0, len(p)):
         value = p[i]
         if value > max_value:
-            value = max_value
+            max_value = value
             max_index = i
     return max_index
 
